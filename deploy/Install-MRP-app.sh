cabf0126cc9188eacfdb7747a6f50fa9509b5465
devopsdemos@outlook.com <devopsdemos@outlook.com>
2015-09-23 00:45:32 +0530

    fixes to container logic

--- deploy/Install-MRP-app.sh
+++ deploy/Install-MRP-app.sh
@@ -18,20 +18,20 @@ shift #past argument or value
 done
 
 # Kill java to stop current website
-sudo pkill -9 'java'
+pkill -9 'java'
 
 # Remove old artifacts
-sudo rm -f /var/lib/partsunlimited/MongoRecords.js*
-sudo rm -f /var/lib/partsunlimited/mrp.war*
-sudo rm -f /var/lib/partsunlimited/ordering-service-0.1.0.jar*
+rm -f /var/lib/partsunlimited/MongoRecords.js*
+rm -f /var/lib/partsunlimited/mrp.war*
+rm -f /var/lib/partsunlimited/ordering-service-0.1.0.jar*
 
 # Install packages
-sudo apt-get update
-sudo apt-get install openjdk-8-jdk -y
-sudo apt-get install openjdk-8-jre -y
-sudo apt-get install mongodb -y
-sudo apt-get install tomcat7 -y
-sudo apt-get install wget -y
+apt-get update
+apt-get install openjdk-8-jdk -y
+apt-get install openjdk-8-jre -y
+apt-get install mongodb -y
+apt-get install tomcat7 -y
+apt-get install wget -y
 
 # Set Java environment variables
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
@@ -41,7 +41,7 @@ export PATH=$PATH:/usr/lib/jvm/java-8-openjdk-amd64/bin
 AzureResource="https://$DropStorageAccountName.blob.core.windows.net/$DropContainerName/"
 
 # Download MongoRecords.js from Azure blog storage
-sudo wget ${AzureResource}MongoRecords.js -P /var/lib/partsunlimited
+wget ${AzureResource}MongoRecords.js -P /var/lib/partsunlimited
 
 # Wait for 10 seconds to make sure previous step is completed
 sleep 10
@@ -50,32 +50,32 @@ sleep 10
 mongo ordering /var/lib/partsunlimited/MongoRecords.js
 
 # Change Tomcat listening port from 8080 to 9080
-sudo sed -i s/8080/9080/g /etc/tomcat7/server.xml
+sed -i s/8080/9080/g /etc/tomcat7/server.xml
 
 # Download the client WAR file
-sudo wget ${AzureResource}mrp.war -P /var/lib/partsunlimited/
+wget ${AzureResource}mrp.war -P /var/lib/partsunlimited/
 
 # Wait for 10 seconds to make sure previous step is completed
 sleep 10
 
 # Copy WAR file to Tomcat directory for auto-deployment
-sudo /etc/init.d/tomcat7 stop
+/etc/init.d/tomcat7 stop
 
 sleep 5
 
-sudo rm -rf /var/lib/tomcat7/webapps/mrp*
-sudo cp /var/lib/partsunlimited/mrp.war /var/lib/tomcat7/webapps
+rm -rf /var/lib/tomcat7/webapps/mrp*
+cp /var/lib/partsunlimited/mrp.war /var/lib/tomcat7/webapps
 
 # Restart Tomcat
-sudo /etc/init.d/tomcat7 restart
+/etc/init.d/tomcat7 restart
 
 # Download the Ordering Service jar from Azure storage
-sudo wget ${AzureResource}ordering-service-0.1.0.jar -P /var/lib/partsunlimited/
+wget ${AzureResource}ordering-service-0.1.0.jar -P /var/lib/partsunlimited/
 
 # Wait for 20 seconds to make sure previous step is completed
 sleep 20
 
 # Run Ordering Service app
-sudo java -jar /var/lib/partsunlimited/ordering-service-0.1.0.jar &
+java -jar /var/lib/partsunlimited/ordering-service-0.1.0.jar &
 
 echo "MRP application successfully deployed. Go to http://$HOSTNAME.cloudapp.net:9080/mrp"
