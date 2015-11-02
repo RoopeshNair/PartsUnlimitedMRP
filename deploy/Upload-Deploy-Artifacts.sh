af3f132df69991b40a4d9af83ca7e228303f4da9
RoopeshNair <roopesh7@hotmail.com>
2015-11-02 16:04:44 +0530

    Update Upload-Deploy-Artifacts.sh

--- deploy/Upload-Deploy-Artifacts.sh
+++ deploy/Upload-Deploy-Artifacts.sh
@@ -45,23 +45,22 @@ echo CONTAINER			= "${CONTAINER}"
 echo DROP FOLDER		= "${DROPFOLDER}"
 echo SOURCES FOLDER		= "${SOURCESFOLDER}"
 echo VIRTUAL MACHINE		= "${VIRTUALMACHINE}"
-echo PWD		= "${TOMPWD}"
+#echo PWD		= "${TOMPWD}"
 
 # Delete previous blobs Usage: storage blob delete [options] [container] [blob]
-azure storage blob delete -q -a "${STORAGEACCOUNT}" -k "${STORAGEKEY}" "${CONTAINER}" "integration-service-0.1.0.jar"
-azure storage blob delete -q -a "${STORAGEACCOUNT}" -k "${STORAGEKEY}" "${CONTAINER}" "ordering-service-0.1.0.jar"
-azure storage blob delete -q -a "${STORAGEACCOUNT}" -k "${STORAGEKEY}" "${CONTAINER}" "mrp.war"
-azure storage blob delete -q -a "${STORAGEACCOUNT}" -k "${STORAGEKEY}" "${CONTAINER}" "MongoRecords.js"
-azure storage blob delete -q -a "${STORAGEACCOUNT}" -k "${STORAGEKEY}" "${CONTAINER}" "Install-MRP-app.sh"
-
-echo "Deleted prior artifact version from " "${CONTAINER}"
+#azure storage blob delete -q -a "${STORAGEACCOUNT}" -k "${STORAGEKEY}" "${CONTAINER}" "integration-service-0.1.0.jar"
+#azure storage blob delete -q -a "${STORAGEACCOUNT}" -k "${STORAGEKEY}" "${CONTAINER}" "ordering-service-0.1.0.jar"
+#azure storage blob delete -q -a "${STORAGEACCOUNT}" -k "${STORAGEKEY}" "${CONTAINER}" "mrp.war"
+#azure storage blob delete -q -a "${STORAGEACCOUNT}" -k "${STORAGEKEY}" "${CONTAINER}" "MongoRecords.js"
+#azure storage blob delete -q -a "${STORAGEACCOUNT}" -k "${STORAGEKEY}" "${CONTAINER}" "Install-MRP-app.sh"
+#echo "Deleted prior artifact version from " "${CONTAINER}"
 
 # Upload necessary files to blob storage
-azure storage blob upload -q -a "${STORAGEACCOUNT}" -k "${STORAGEKEY}" "${DROPFOLDER}/drop/Backend/IntegrationService/build/libs/integration-service-0.1.0.jar" "${CONTAINER}"
-azure storage blob upload -q -a "${STORAGEACCOUNT}" -k "${STORAGEKEY}" "${DROPFOLDER}/drop/Backend/OrderService/build/libs/ordering-service-0.1.0.jar" "${CONTAINER}"
-azure storage blob upload -q -a "${STORAGEACCOUNT}" -k "${STORAGEKEY}" "${DROPFOLDER}/drop/Clients/build/libs/mrp.war" "${CONTAINER}"
-azure storage blob upload -q -a "${STORAGEACCOUNT}" -k "${STORAGEKEY}" "${SOURCESFOLDER}/deploy/MongoRecords.js" "${CONTAINER}"
-azure storage blob upload -q -a "${STORAGEACCOUNT}" -k "${STORAGEKEY}" "${SOURCESFOLDER}/deploy/Install-MRP-app.sh" "${CONTAINER}"
+#azure storage blob upload -q -a "${STORAGEACCOUNT}" -k "${STORAGEKEY}" "${DROPFOLDER}/drop/Backend/IntegrationService/build/libs/integration-service-0.1.0.jar" "${CONTAINER}"
+#azure storage blob upload -q -a "${STORAGEACCOUNT}" -k "${STORAGEKEY}" "${DROPFOLDER}/drop/Backend/OrderService/build/libs/ordering-service-0.1.0.jar" "${CONTAINER}"
+#azure storage blob upload -q -a "${STORAGEACCOUNT}" -k "${STORAGEKEY}" "${DROPFOLDER}/drop/Clients/build/libs/mrp.war" "${CONTAINER}"
+#azure storage blob upload -q -a "${STORAGEACCOUNT}" -k "${STORAGEKEY}" "${SOURCESFOLDER}/deploy/MongoRecords.js" "${CONTAINER}"
+#azure storage blob upload -q -a "${STORAGEACCOUNT}" -k "${STORAGEKEY}" "${SOURCESFOLDER}/deploy/Install-MRP-app.sh" "${CONTAINER}"
 
 # Use CustomScriptForLinux extension to deploy the app on VM
 SCRIPTURI="https://$STORAGEACCOUNT.blob.core.windows.net/$CONTAINER/Install-MRP-app.sh"
