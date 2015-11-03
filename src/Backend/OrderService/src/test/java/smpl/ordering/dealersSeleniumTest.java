b81c3179550071285aef28f1987514433f278aad
roopesh.nair@outlook.com <roopesh.nair@outlook.com>
2015-11-03 15:05:26 +0530

    imports

--- src/Backend/OrderService/src/test/java/smpl/ordering/dealersSeleniumTest.java
+++ src/Backend/OrderService/src/test/java/smpl/ordering/dealersSeleniumTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.*;
 import java.util.concurrent.TimeUnit;
 import java.util.Date;
 import java.io.File;
-import org.openqa.selenium.support.ui.Select;
+//import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
